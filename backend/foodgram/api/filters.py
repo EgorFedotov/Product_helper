f17d4033ee3c0372351643e63811000c4e120ce9
EgorFedotov <egorfedotovarz@gmail.com>
2023-06-08 14:59:51 +0300

    workflow

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -17,8 +17,8 @@ class RecipeFilter(filters.FilterSet):
 
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.NumberFilter(
-            method='filter_is_in_shopping_cart'
-        )
+        method='filter_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
