c5466252374bd109833bb8d1e53f907a71ee1ac8
EgorFedotov <egorfedotovarz@gmail.com>
2023-06-10 15:51:44 +0300

    ingridients

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -11,6 +11,7 @@ from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from users.models import User
 
 from .filters import IngredientFilter, RecipeFilter
