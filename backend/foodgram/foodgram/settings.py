180cf2d09802f32b391d911aa8459c6b027cbc6f
EgorFedotov <egorfedotovarz@gmail.com>
2023-06-11 15:29:34 +0300

    image

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -124,7 +124,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
-    'UPLOADED_FILES_USE_URL': False,
 }
 
 DJOSER = {
